32f8a257eb70f3406be98f58afbd0361f4c36016
extreme4all <40169115+extreme4all@users.noreply.github.com>
2023-10-10 21:57:21 +0200

    added pytest

--- src/AioKafkaEngine/AioKafkaEngine.py
+++ src/AioKafkaEngine/AioKafkaEngine.py
@@ -6,6 +6,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def retry_on_exception(max_retries=3, retry_interval=5):
     def decorator(func):
         async def wrapper(*args, **kwargs):
@@ -13,58 +14,77 @@ def retry_on_exception(max_retries=3, retry_interval=5):
             while retries < max_retries:
                 try:
                     await func(*args, **kwargs)
+                    return wrapper
                 except Exception as e:
                     logger.error(f"Error: {e}")
-                    retries += 1
                     await asyncio.sleep(retry_interval)
-                else:
-                    break
+                retries += 1
+
         return wrapper
+
     return decorator
+
+
+def async_background_task(func):
+    async def wrapper(*args, **kwargs):
+        task = asyncio.ensure_future(func(*args, **kwargs))
+        return task
+
+    return wrapper
+
 
 class AioKafkaEngine:
     consumer = None
     producer = None
-    receive_queue = Queue()
-    send_queue = Queue()
 
-    def __init__(self, bootstrap_servers: list[str], topic: str):
+    def __init__(
+        self,
+        bootstrap_servers: list[str],
+        topic: str,
+        send_queue_size: int = 100,
+        receive_queue_size: int = 100,
+    ):
         self.bootstrap_servers = bootstrap_servers
         self.topic = topic
+        self.receive_queue = Queue(maxsize=receive_queue_size)
+        self.send_queue = Queue(maxsize=send_queue_size)
 
-    async def start_consumer(self, group_id: str):
+    async def start_consumer(self, group_id: str) -> Queue:
         self.consumer = AIOKafkaConsumer(
             self.topic,
             bootstrap_servers=self.bootstrap_servers,
-            value_serializer=lambda v: json.dumps(v).encode(),
             group_id=group_id,
+            value_deserializer=lambda x: json.loads(x.decode('utf-8'))
         )
         await self.consumer.start()
-        self.consumer
 
     async def start_producer(self):
         self.producer = AIOKafkaProducer(
             bootstrap_servers=self.bootstrap_servers,
-            value_serializer=lambda v: json.dumps(v).encode()
+            value_serializer=lambda v: json.dumps(v).encode(),
         )
         await self.producer.start()
 
-    @retry_on_exception(max_retries=3, retry_interval=5)
+    @async_background_task
+    @retry_on_exception()
     async def consume_messages(self):
         if self.consumer is None:
             raise ValueError("Consumer not started. Call start_consumer() first.")
 
         async for message in self.consumer:
             value = message.value
-            self.receive_queue.put(value)
-    
-    @retry_on_exception(max_retries=3, retry_interval=5)
+            await self.receive_queue.put(value)
+
+    @async_background_task
+    @retry_on_exception()
     async def produce_messages(self):
         if self.producer is None:
             raise ValueError("Producer not started. Call start_producer() first.")
+
         while True:
             message = await self.send_queue.get()
             await self.producer.send(self.topic, value=message)
+            self.send_queue.task_done()
 
     async def stop_consumer(self):
         if self.consumer:
@@ -73,6 +93,6 @@ class AioKafkaEngine:
     async def stop_producer(self):
         if self.producer:
             await self.producer.stop()
-    
+
     def is_ready(self):
         return self.consumer is not None or self.producer is not None
